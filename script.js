5bb671e4ed8d9804e8e59c3dc2c14495bc752611
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2023-10-03 21:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+var findChar = function(str){
+  var char;
+
+  for(var i = 0; i < str.length; i++){
+    if(str.indexOf(str[i]) === str.lastIndexOf(str[i])){
+      char = str[i];
+      break;
+    }
+  }
+
+  return char;
+}
+
+$(document).ready(function(){
+  $('#result').append(findChar("abacdsdbec"));
+})
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
