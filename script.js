1a4fa95e31d91dea83e864101211cdac3727bad6
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2023-10-05 15:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function firstNonRepeatedChar(str) {
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
-      return char;
-    }
-  }
-  return "_";
+
+	let obj={};
+	for(let i=0;i<str.length;i++)
+		{
+			if(obj[str[i]]!==undefined){
+				obj[str[i]]+=1;
+			}
+			else{
+				obj[str[i]]==1;
+			}
+		}
+	for(let i=0;i<str.length;i++)
+		{
+			if(obj[str[i]]==1){
+				return str[i];
+			}
+       }
+  return null;
 }
 	
 	
